ec846b104a2a3a414c5d08af297537718852294a
Sarah Baghdadi <sera.baghdadi@gmail.com>
2021-07-31 17:30:09 -0400

    Add class names to key buttons

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@
 
 const game = new Game(); // This line.
 
+// Add class names to buttons
+let keys = document.querySelectorAll('.key');
+keys.forEach(key => key.classList.add(`${key.textContent}`))
+
 // Start button starts game.
 document.querySelector('#btn__reset').addEventListener('click', (e) => {
     game.startGame();
@@ -13,6 +17,14 @@ document.querySelector('#btn__reset').addEventListener('click', (e) => {
 // Clicking on a letter key calls handle interaction.
 qwerty.addEventListener('click', (e) => {
     if (e.target.className == 'key') {
+        console.log(e);
         game.handleInteraction(e);
     }
 });
+
+window.addEventListener("keydown", (e) => {
+    console.log(e);
+    //game.handleInteraction(e.key)
+    // Cancel the default action to avoid it being handled twice
+    e.preventDefault();
+  }, true);
